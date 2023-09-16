4d912543ef9360c8bbba2f7ac04bf4adb8eaa022
litsu987 <navioaleix@gmail.com>
2023-09-16 02:56:42 +0200

    Punto4

--- barco.php
+++ barco.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,6 +9,9 @@
         	border: 1px black solid;
         	border-collapse: collapse;
     	}
+        .submarine {
+            background-color: blue; /* Puedes personalizar el estilo del submarino */
+        }
 	</style>
 </head>
 <body>
@@ -19,26 +21,53 @@
 	<table>
 
 	<?php
-	$N = 8;
-	$M = 6;
+	$N = 8; // Número de columnas
+	$M = 6; // Número de filas
 	$letra = "A";
+
+	$barcos = array(
+		array("tipo" => "fragata", "longitud" => 1),
+		array("tipo" => "submarí", "longitud" => 2),
+		array("tipo" => "destructor", "longitud" => 3),
+		array("tipo" => "portaavions", "longitud" => 4)
+	);
+	
+	// Define la longitud del submarino
+	$longitudSubmarino = 2;
+
+	// Define la orientación del submarino (horizontal o vertical)
+	$orientacion = "horizontal"; // Cambia a "vertical" para probar en vertical
+
 	for ($i = 0; $i < $M; $i++) {
-     	echo " <tr>\n";
-     	for ($j = 0; $j < $N; $j++) {
-        	if ($j == 0 && $i == 0) {
-            	echo "\t\t<td> </td>\n";
-        	} elseif ($j == 0) {
-            	echo "\t\t<td>$letra</td>\n";
-        	} elseif ($i == 0) {
-            	echo "\t\t<td>$j</td>\n";
-        	} else {
-            	echo "\t\t<td>$letra$j</td>\n";
-        	}
-    	}
-    	if ($i > 0) {
-        	$letra++;
-    	}
-    	echo " </tr>";
+		echo " <tr>\n";
+		for ($j = 0; $j < $N; $j++) {
+			if ($j == 0 && $i == 0) {
+				echo "\t\t<td> </td>\n";
+			} elseif ($j == 0) {
+				echo "\t\t<td>$letra</td>\n";
+			} elseif ($i == 0) {
+				echo "\t\t<td>$j</td>\n";
+			} else {
+				// Comprueba si esta celda es parte del barco
+				$barcoEncontrado = false;
+				foreach ($barcos as $barco) {
+					if ($orientacion == "horizontal" && $j >= 1 && $j <= $barco["longitud"] && $i == 1 && $barcoEncontrado == false) {
+						echo "\t\t<td class='ship'>" . substr($barco["tipo"], 0, 1) . "</td>\n";
+						$barcoEncontrado = true;
+					} elseif ($orientacion == "vertical" && $i >= 1 && $i <= $barco["longitud"] && $j == 1 && $barcoEncontrado == false) {
+						echo "\t\t<td class='ship'>" . substr($barco["tipo"], 0, 1) . "</td>\n";
+						$barcoEncontrado = true;
+					}
+				}
+				if ($barcoEncontrado == false) {
+					echo "\t\t<td></td>\n";
+				}
+			}
+		}
+		if ($i > 0) {
+			$letra++;
+		}
+		echo " </tr>";
 	}
 	?>
 	</table>
